aa4ee0e8fac760ccd627cd750d81e5f7a5201843
Roshan Thomas <wordexplorer@users.noreply.github.com>
2025-12-30 16:20:31 -0500

    LeetCode 876. Middle of the Linked List

--- src/main/java/leetcode/learning/FindMiddleElementInLinkedList.java
+++ src/main/java/leetcode/learning/FindMiddleElementInLinkedList.java
@@ -1,28 +1,56 @@
 package leetcode.learning;
 
-    //Definition for singly-linked list.
-      class ListNode {
-          int val;
-          ListNode next;
-          ListNode() {}
-          ListNode(int val) { this.val = val; }
-          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     }
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
 
-    public class FindMiddleElementInLinkedList {
-
-        public ListNode middleNode(ListNode head) {
-
-            ListNode slow = head;
-            ListNode fast = head;
-
-            while(fast != null && fast.next !=null){
-                slow = slow.next;
-                fast = fast.next.next;
-            }
-
-            return slow;
-
-        }
+    ListNode() {
     }
 
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class FindMiddleElementInLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+
+    }
+
+    static ListNode build(int... vals) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        for (int v : vals) {
+            curr.next = new ListNode(v);
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+
+        FindMiddleElementInLinkedList solver = new FindMiddleElementInLinkedList();
+        ListNode head = build(1, 2, 3, 4, 5);
+        ListNode mid = solver.middleNode(head);
+        System.out.println(mid.val);
+
+    }
+
+}
